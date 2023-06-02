246498874b1a627a92a84a37fc344a351ed098e9
de.Beast <litvinov.d.s100@gmail.com>
2023-06-02 11:41:45 +0300

    сам дамб щит

--- Client/src/GameWindow.py
+++ Client/src/GameWindow.py
@@ -1,15 +1,15 @@
 import random
 
+from AllUi.ui_Game import Ui_Game
 from PySide6 import QtCore, QtGui, QtWidgets
 from PySide6.QtGui import QMovie
 
-from AllUi.ui_Game import Ui_Game
-from Shared.games.roulette.enums import RouletteColor
-from Shared.games.enums import GameType
+from account.api import AccountAPI, AccountInfo
 from chat.api import ChatAPI
-from account.api import AccountInfo
-from account.api import AccountAPI
 from games.roulette.api import RouletteAPI, RouletteBet, RouletteState
+from Shared.games.enums import GameType
+from Shared.games.roulette.enums import RouletteColor
+
 
 class GameWindow(object):
     def __init__(self):
@@ -27,15 +27,16 @@ class GameWindow(object):
         self.rouletteAPI = RouletteAPI()
         self.rouletteAPI.betRecieved.connect(self.OnBetTaken)
         self.rouletteAPI.resultRecieved.connect(self.UpdateRouletteState)
+        self.rouletteAPI.rouletteStateChanged.connect(self.UpdateRouletteStatus)
 
         self.blackBet = 0
         self.redBet = 0
         self.greenBet = 0
 
-        #TODO connect dis shit
-        self.UI.blackBetButton.clicked.connect(lambda : self.OnBetMaked(RouletteColor.BLACK))
-        self.UI.redBetButton.clicked.connect(lambda : self.OnBetMaked(RouletteColor.RED))
-        self.UI.greenBetButton.clicked.connect(lambda : self.OnBetMaked(RouletteColor.GREEN))
+        # TODO connect dis shit
+        self.UI.blackBetButton.clicked.connect(lambda: self.OnBetMaked(RouletteColor.BLACK))
+        self.UI.redBetButton.clicked.connect(lambda: self.OnBetMaked(RouletteColor.RED))
+        self.UI.greenBetButton.clicked.connect(lambda: self.OnBetMaked(RouletteColor.GREEN))
 
         self.RegisterGifs()
 
@@ -48,34 +49,48 @@ class GameWindow(object):
         self.UI.balanceLabel.setText("БАЛАНС: 21")
 
     def UpdateAccountInfo(self, newInfo):
-        self.accountInfo = newInfo
-        self.UI.balanceLabel.setText("БАЛАНС: " + str(newInfo['balance']))
+        self.UI.balanceLabel.setText("БАЛАНС: " + str(newInfo["balance"]))
 
-    def UpdateRouletteState(self, color : RouletteColor, num : int):
+    def UpdateRouletteState(self, color: RouletteColor, num: int):
         self.PlayGif(num)
 
-    def UpdateRouletteStatus(self, status : RouletteState):
+    def UpdateRouletteStatus(self, status: RouletteState):
         if status == RouletteState.SPINNING:
             self.PlayGif(0)
         elif status == RouletteState.STOPPED:
-            pass
+            self.accountAPI.get_account_info()
+            self.ClearBets()
 
-    def RegisterGifs(self):
-        self.gifs = []
-        self.gifs.append(QMovie("gifs//infspinning.gif")) #0
-        self.gifs.append(QMovie("gifs//black1-stop.gif")) #1
-        self.gifs.append(QMovie("gifs//black2-stop.gif")) #2
-        self.gifs.append(QMovie("gifs//black3-stop.gif")) #3
-        self.gifs.append(QMovie("gifs//red1-stop.gif"))   #4
-        self.gifs.append(QMovie("gifs//red2-stop.gif"))   #5
-        self.gifs.append(QMovie("gifs//red3-stop.gif"))   #6
-        self.gifs.append(QMovie("gifs//zero-stop.gif"))   #7
-        self.gifs.append(QMovie("gifs//zero2-stop.gif"))  #8
+    def RegisterGifs(self) -> None:
+        self.gifs: list[QMovie] = []
+        self.gifs.append(QMovie("./gifs/infspinning.gif"))  # 0
+        self.gifs.append(QMovie("./gifs/black1-stop.gif"))  # 1
+        self.gifs.append(QMovie("./gifs/black2-stop.gif"))  # 2
+        self.gifs.append(QMovie("./gifs/black3-stop.gif"))  # 3
+        self.gifs.append(QMovie("./gifs/red1-stop.gif"))  # 4
+        self.gifs.append(QMovie("./gifs/red2-stop.gif"))  # 5
+        self.gifs.append(QMovie("./gifs/red3-stop.gif"))  # 6
+        self.gifs.append(QMovie("./gifs/zero-stop.gif"))  # 7
+        self.gifs.append(QMovie("./gifs/zero2-stop.gif"))  # 8
 
-    def OnBetMaked(self, type : RouletteColor):
-        self.rouletteAPI.bet(RouletteBet(total=int(self.UI.betEdit.toPlainText()), color=type))
+    def OnBetMaked(self, type: RouletteColor):
+        bet = RouletteBet(int(self.UI.betEdit.toPlainText()), type)
+        self.rouletteAPI.bet(bet)
 
-    def OnBetTaken(self, nick : str, bet : RouletteBet):
+    def ClearBets(self):
+        self.UI.redBets.clear()
+        self.redBet = 0
+        self.UI.redBetsSum.setText("ОБЩАЯ СТАВКА: " + str(self.redBet))
+
+        self.UI.blackBets.clear()
+        self.blackBet = 0
+        self.UI.blackBetsSum.setText("ОБЩАЯ СТАВКА: " + str(self.blackBet))
+
+        self.UI.greenBets.clear()
+        self.greenBet = 0
+        self.UI.greenBetsSum.setText("ОБЩАЯ СТАВКА: " + str(self.greenBet))
+
+    def OnBetTaken(self, nick: str, bet: RouletteBet):
         if bet.color == RouletteColor.RED:
             betSum = self.UI.redBetsSum
             betCont = self.UI.redBets
@@ -92,11 +107,10 @@ class GameWindow(object):
             betCont = self.UI.greenBets
             betSize = self.greenBet
 
-
         betSum.setText("ОБЩАЯ СТАВКА: " + str(betSize))
         betCont.append("<b>" + nick + ":</b> " + str(bet.total))
 
-    def OnMessageRecieved(self, nickname : str, message : str):
+    def OnMessageRecieved(self, nickname: str, message: str):
         self.UI.chat.append("<b>" + nickname + ":</b>")
         self.UI.chat.append(message)
         self.UI.chat.append("")
@@ -108,6 +122,6 @@ class GameWindow(object):
         self.chatApi.send_message(self.UI.messageEdit.text())
         self.UI.messageEdit.clear()
 
-    def PlayGif(self, indOfGif : int):
-        self.UI.circle.setMovie(self.gifs[indOfGif]) #TODO create movie from MaxGrig's gif
+    def PlayGif(self, indOfGif: int):
+        self.UI.circle.setMovie(self.gifs[indOfGif])  # TODO create movie from MaxGrig's gif
         self.gifs[indOfGif].start()
